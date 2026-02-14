7da4162d083b110c079f7b81689798b09f3119c4
suthartarak <suthartarak@suthartaraks-MacBook-Air.local>
2026-02-14 20:29:06 +0530

    check

--- src/main/java/com/tarak/dsa/a_intermediate/a_arrays/D_SpecialIndex.java
+++ src/main/java/com/tarak/dsa/a_intermediate/a_arrays/D_SpecialIndex.java
@@ -3,7 +3,7 @@ package com.tarak.dsa.a_intermediate.a_arrays;
 public class D_SpecialIndex {
     public static void main(String[] args) {
         int[] arr = {};
-
+        System.out.println("hello");
     }
 
     private void solve(int[] arr) {
